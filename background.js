d1745197ea896cd9f0202acae6cf421a5d73e158
Nicholas Devlin <nicholas.devlin@Alfredo.local>
2018-06-05 18:42:07 -0400

    move options to popup

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: '#3aa757' }, function () {
-    console.log("The color is green.");
+  chrome.storage.sync.set({ kButtonColors: ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1', 'transparent'] }, function () {
+    console.log("kButtonColors set");
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
--- popup.js
+++ popup.js
@@ -1,15 +1,20 @@
-let changeColor = document.getElementById('changeColor');
+let buttonDiv = document.getElementById('buttonDiv');
+function constructOptions(kButtonColors) {
+    for (let color of kButtonColors) {
+        let button = document.createElement('button');
+        button.style.backgroundColor = color;
+        button.setAttribute('value', color);
+        button.addEventListener('click', function () {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    { code: 'document.body.style.backgroundColor = "' + color + '";' });
+            });
+        });
+        buttonDiv.appendChild(button);
+    }
+}
 
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get('kButtonColors', function (data) {
+    constructOptions(data.kButtonColors);
 });
-
-changeColor.onclick = function (element) {
-    let color = element.target.value;
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { code: 'document.body.style.backgroundColor = "' + color + '";' });
-    });
-};
