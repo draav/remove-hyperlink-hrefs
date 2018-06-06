9c733e24ac3747db4d6b65b008bbe2b68b83163a
Nicholas Devlin <nicholas.devlin@Alfredo.fios-router.home>
2018-06-06 00:42:53 -0400

    remove default stuff, add persistant checkbox

--- background.js
+++ background.js
@@ -1,14 +1,3 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ kButtonColors: ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1', 'transparent'] }, function () {
-    console.log("kButtonColors set");
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'developer.chrome.com' },
-      })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+  chrome.storage.sync.set({ isEnabled: true });
 });
--- popup.js
+++ popup.js
@@ -1,20 +1,23 @@
-let buttonDiv = document.getElementById('buttonDiv');
-function constructOptions(kButtonColors) {
-    for (let color of kButtonColors) {
-        let button = document.createElement('button');
-        button.style.backgroundColor = color;
-        button.setAttribute('value', color);
-        button.addEventListener('click', function () {
-            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                chrome.tabs.executeScript(
-                    tabs[0].id,
-                    { code: 'document.body.style.backgroundColor = "' + color + '";' });
-            });
+'use strict';
+// Saves options to chrome.storage
+function toggle_enable() {
+    var isEnabledNew = document.getElementById('enabledCheckBox').checked;
+    chrome.storage.sync.set({ isEnabled: isEnabledNew },
+        function () {
+            // Update status to let user know options were saved.
+            var status = document.getElementById('status');
+            status.textContent = 'Options saved.';
+            setTimeout(function () {
+                status.textContent = '';
+            }, 750);
         });
-        buttonDiv.appendChild(button);
-    }
 }
 
-chrome.storage.sync.get('kButtonColors', function (data) {
-    constructOptions(data.kButtonColors);
-});
+function restore_options() {
+    chrome.storage.sync.get('isEnabled', function (data) {
+        document.getElementById('enabledCheckBox').checked = data.isEnabled;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('enabledCheckBox').addEventListener('click', toggle_enable);
