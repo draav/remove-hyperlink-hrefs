8988b767c46c215bc12f4efb7720f3533204e2c1
Nicholas Devlin <nicholas.devlin@Alfredo.fios-router.home>
2018-06-09 20:50:26 -0400

    add mutationobserver to capture added links

--- content.js
+++ content.js
@@ -1,12 +1,58 @@
 'use strict';
 
+
+
+// when extension is enabled, remove hrefs
+// store the values in [removed-href] so it can be put back later
+
+function remove_hrefs() {
+    let links = document.body.querySelectorAll('a[href]');
+    console.log(links);
+    for (let link of links) {
+        link.setAttribute('removed-href', link.getAttribute('href'));
+        link.removeAttribute('href');
+    }
+}
+
+// when extension is disabled, put the hrefs back
+function put_hrefs_back() {
+    let links = document.querySelectorAll('[removed-href]');
+    console.log(links);
+    for (let link of links) {
+        link.setAttribute('href', link.getAttribute('removed-href'));
+        link.removeAttribute('removed-href');
+    }
+}
+
+
+// creates a Mutation observer that will remove hrefs whenever page is loaded
+// https://hacks.mozilla.org/2012/05/dom-mutationobserver-reacting-to-dom-changes-without-killing-browser-performance/
+var observer = new MutationObserver(function (mutations) {
+    for(let mutation of mutations) {
+        remove_hrefs();
+        console.log(mutation);
+    }
+});
+// https://developer.mozilla.org/en-US/docs/Web/API/MutationObserverInit
+var observerConfig = {
+    attributes: true,
+    childList: true,
+    characterData: true
+};
+
+// if extension isEnabled remove hrefs in document, 
+// then enable MutationObserver to remove any future hrefs added
 chrome.storage.sync.get('isEnabled', function (data) {
-    if(!data.isEnabled) {
+    if (!data.isEnabled) {
         return;
     }
 
-    for (let link of document.links) {
-        link.removeAttribute('href');
-        console.log(link);
-    }
+    // run once on startup
+    remove_hrefs();
+    // run every time dom is updated
+    observer.observe(document.body, observerConfig);
+
+    // NOTE: may want to only trigger on whole doc once, then on just the new nodes later
+    //       right now it run on the entire doc after every update
+    //       check performance then optomize if it's a problem
 });
--- popup.js
+++ popup.js
@@ -1,18 +1,27 @@
 'use strict';
-// Saves options to chrome.storage
+
+// Saves options to chrome.storage whenever changed
 function toggle_enable() {
     var isEnabledNew = document.getElementById('enabledCheckBox').checked;
     chrome.storage.sync.set({ isEnabled: isEnabledNew },
         function () {
-            // Update status to let user know options were saved.
+            //display message showing it worked
             var status = document.getElementById('status');
             status.textContent = 'Options saved.';
             setTimeout(function () {
                 status.textContent = '';
             }, 750);
+
+            // send message to all tabs that extension have been toggled
+            // chrome.tabs.query({}, function(tabs) {
+            //     for(let tab of tabs) {
+            //         chrome.tabs.sendMessage(tab.id, isEnabledNew);
+            //     }
+            // });
         });
 }
 
+// sets popup options to wahtever is in chrome storage
 function restore_options() {
     chrome.storage.sync.get('isEnabled', function (data) {
         document.getElementById('enabledCheckBox').checked = data.isEnabled;
